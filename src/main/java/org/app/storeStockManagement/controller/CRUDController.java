b3319a9d8f5662b442a64dc3c65ad49aba2a4465
Arsha R Unnithan <arsharunnithan@gmail.com>
2023-08-22 15:01:30 +0530

    pushing changes

--- src/main/java/org/app/storeStockManagement/controller/CRUDController.java
+++ src/main/java/org/app/storeStockManagement/controller/CRUDController.java
@@ -1,11 +1,14 @@
 package org.app.storeStockManagement.controller;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 import org.app.storeStockManagement.model.Product;
 import org.app.storeStockManagement.service.CRUDService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,10 +46,17 @@ public class CRUDController {
 	public Product fetchProductById(@PathVariable int id){
 		return crudService.fetchProductById(id).get();
 	}
+	
 	@DeleteMapping("/deleteproductbyid/{id}")
 	@CrossOrigin(origins = "http://localhost:4200")
-	public String deleteProductById(@PathVariable int id){
-		return crudService.deleteProductById(id);
+	public ResponseEntity<String> deleteProductById(@PathVariable int id){
+	    String result = crudService.deleteProductById(id);
+	    if (result.equals("Product successfully deleted")) {
+	        return ResponseEntity.ok().body("{\"message\": \"" + result + "\"}");
+	    } else {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"" + result + "\"}");
+	    }
 	}
+
 	
 }
